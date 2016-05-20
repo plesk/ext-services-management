6f56e95388a0caa582cab5de759fc58b2695285b
Aleksei Vokhmianin <avokhmianin@odin.com>
2016-05-20 13:12:35 +0600

    initial import

---
+++ plib/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+// Copyright 1999-2016. Parallels IP Holdings GmbH.
+class IndexController extends pm_Controller_Action
+{
+    protected $_accessLevel = 'admin';
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
---
+++ plib/hooks/SystemServices.php
@@ -0,0 +1,79 @@
+<?php
+// Copyright 1999-2015. Parallels IP Holdings GmbH. All Rights Reserved.
+
+class Modules_CustomService_Service extends pm_SystemService_Service
+{
+
+    public function getName()
+    {
+        return 'My Custom Service SDK Example';
+    }
+
+    public function getId ()
+    {
+        return 'customservice';
+    }
+
+    public function onStart()
+    {
+        $result = pm_ApiCli::callSbin('mycustomserviceconsole', array('start'));
+        if ($result['code'] !== 0) {
+            throw new pm_Exception ('Error occured when starting My custom service.');
+        }
+    }
+
+    public function onStop()
+    {
+        $result = pm_ApiCli::callSbin('mycustomserviceconsole', array('stop'));
+        if ($result['code'] !== 0) {
+            throw new pm_Exception ('Error occured when stopping My custom service.');
+        }
+    }
+
+    public function onRestart()
+    {
+        $result = pm_ApiCli::callSbin('mycustomserviceconsole', array('restart'));
+        if ($result['code'] !== 0) {
+            throw new pm_Exception ('Error occured when restarting My custom service.');
+        }
+    }
+
+    public function isRunning()
+    {
+        $result = pm_ApiCli::callSbin('mycustomserviceconsole', array('status'));
+        if ($result['code'] == 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function isConfigured()
+    {
+        $result = pm_ApiCli::callSbin('mycustomserviceconsole', array('isconfigured'));
+        if ($result['code'] == 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function isInstalled()
+    {
+        $result = pm_ApiCli::callSbin('mycustomserviceconsole', array('isinstalled'));
+        if ($result['code'] == 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+    
+}
+
+class Modules_CustomService_SystemServices extends pm_Hook_SystemServices
+{
+    public function getServices()
+    {
+        return [new Modules_CustomService_Service()];
+    }
+}
