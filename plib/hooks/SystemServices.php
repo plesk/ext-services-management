a9ef875647a01830c06dec30a3045287d1c0a892
avokhmianin <vaazanuda@gmail.com>
2016-05-23 16:01:39 +0600

    Update SystemServices.php
    Copyright fixed

--- plib/hooks/SystemServices.php
+++ plib/hooks/SystemServices.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright 1999-2015. Parallels IP Holdings GmbH. All Rights Reserved.
+// Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
 
 class Modules_CustomService_SystemServices extends pm_Hook_SystemServices
 {
